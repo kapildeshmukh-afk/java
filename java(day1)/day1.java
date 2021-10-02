ac9408a6287e0d741594494bae79e316394397d2
kapildeshmukh-afk <80241134+kapildeshmukh-afk@users.noreply.github.com>
2021-10-02 14:48:15 +0530

    java assignment day 1
    submitted

---
+++ java(day1)/day1.java
@@ -0,0 +1,5 @@
+public class example {
+    public static void main(String[] args){
+        System.out.println("My name is Kapil Deshmukh \n I am studing in Acharaya institute of technology \n Persuing BCA  degree \n ");
+    }
+}
